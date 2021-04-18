3125f151ad6b89191ac4c7c469b1e75fce37ff3a
denisami <denis.sanchez.amieva@gmail.com>
2021-04-18 23:52:25 +0200

    add summary.py

--- summary.py
+++ summary.py
@@ -1,29 +1,26 @@
 import http.server
-import random
 import time
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Summary
 
-REQUEST_INPROGRESS = Gauge('app_requests_inprogress','number of application requests in progress')
-REQUEST_LAST_SERVED = Gauge('app_last_served', 'Time the application was last served.')
+REQUEST_RESPOND_TIME = Summary('app_response_latency_seconds', 'Response latency in seconds')
 
 APP_PORT = 8000
 METRICS_PORT = 8001
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
-    # Directamente hacemos el incremento del valor REQUEST_INPROGRESS cuando la petición entra y el decremento cuando es servida.
-    @REQUEST_INPROGRESS.track_inprogress()
+    @REQUEST_RESPOND_TIME.time()
     def do_GET(self):
-       # REQUEST_INPROGRESS.inc()
-        time.sleep(5)
+        #start_time = time.time()
+        time.sleep(6)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Prometheus-Python application.</center></h2></body></html>", "utf-8"))
         self.wfile.close()
-        REQUEST_LAST_SERVED.set_to_current_time()
-       # REQUEST_LAST_SERVED.set(time.time())
-       #REQUEST_INPROGRESS.dec()
+        #time_taken = time.time() - start_time
+       # REQUEST_RESPOND_TIME.observe(time_taken)
+
 
 if __name__ == "__main__":
     start_http_server(METRICS_PORT)
