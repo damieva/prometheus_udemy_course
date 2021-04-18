c4830cee1e2be783e6319bda35f6a86a2c320d98
denisami <denis.sanchez.amieva@gmail.com>
2021-04-18 23:31:20 +0200

    add gauge.py

--- gauge.py
+++ gauge.py
@@ -1,28 +1,28 @@
 import http.server
 import random
-from prometheus_client import start_http_server, Counter
+import time
+from prometheus_client import start_http_server, Gauge
 
-REQUEST_COUNT = Counter('app_requests_count', 'total app http request count',['app_name', 'endpoint'])
-#               Counter('nombre_de_la_metrica', 'descripcion', labels)
-RANDOM_COUNT = Counter('app_random_count','increment counter by random value')
+REQUEST_INPROGRESS = Gauge('app_requests_inprogress','number of application requests in progress')
+REQUEST_LAST_SERVED = Gauge('app_last_served', 'Time the application was last served.')
 
 APP_PORT = 8000
 METRICS_PORT = 8001
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
+    @REQUEST_INPROGRESS.track_inprogress()
     def do_GET(self):
-        # Incremantemos el valor del contador por cada petición que nos entra
-        REQUEST_COUNT.labels('prom_python_app', self.path).inc()
-        
-        random_val = random.random()*10
-        RANDOM_COUNT.inc(random_val)
-        
+       # REQUEST_INPROGRESS.inc()
+        time.sleep(5)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Prometheus-Python application.</center></h2></body></html>", "utf-8"))
         self.wfile.close()
+        REQUEST_LAST_SERVED.set_to_current_time()
+       # REQUEST_LAST_SERVED.set(time.time())
+       #REQUEST_INPROGRESS.dec()
 
 if __name__ == "__main__":
     start_http_server(METRICS_PORT)
